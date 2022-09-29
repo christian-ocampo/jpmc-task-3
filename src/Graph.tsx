e0d10b2e96916c8e300a785af8215a8748662e81
Christian Ocampo Campuzano <christianocampo@usf.edu>
2022-09-29 18:45:26 -0400

    Ratio changed to compare stocks ABC and DEF Upper and lower limits added when ratio > 110% or < 90% Trigger alert added for clarity on limits being reached

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -23,10 +23,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float', // Tracks the ratio between stocks ABC and DEF.
+      timestamp: 'date', // Ratios are being tracked with respect to time.
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float', // Alerts when the ratio between the stocks crosses the upper or lower bound.
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -36,23 +39,27 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+      elem.setAttribute('row-pivots', '["timestamp"]'); // Lists timestamps along x-axis.
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]'); // Plots only the 
+                                                                                                // selected datapoints.
+      elem.setAttribute('aggregates', JSON.stringify({ // Handles duplicate values by consolidating them into a
+        price_abc: 'avg',                              // single point. Values are considered unique by having
+        price_def: 'avg',                              // a different timestamp. Otherwise, find their average.
+        ratio: 'avg',
         timestamp: 'distinct count',
-      }));
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
+      })); 
     }
   }
 
+  // Executes whenever there is new data.
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ] as unknown as TableData);
     }
   }
 }
